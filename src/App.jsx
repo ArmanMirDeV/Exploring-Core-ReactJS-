cb7b5e27886248cfc440544f55ccd05dd4ad9021
Arman Mir <mirarman8583@gmail.com>
2025-09-14 22:28:17 +0600

    explore jsx and props basics

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,17 @@ function App() {
   return (
     <>
      
-      <h1>Vite + React</h1>
-      <Person> </Person>
-      <Person> </Person>
-      <Sports></Sports>
-      <Person> </Person>
-      <Person> </Person>
-      <Sports> </Sports>
-      <Pet></Pet>
-      <Data></Data>
+      <h1>React Core Concepts</h1>
+      <Student> </Student>
+      <Student> </Student>
+      <Person></Person>
+      <Developer name="Satul Moni" tech="JavaScript"></Developer>
+      <Developer name = "Arman Mir" tech="ReactJS"></Developer>
+      <Developer name = "BongoBolturPola" tech="JaVa"></Developer>
+      <Device device="Phone" model="Samsung F23 5G" brand="Samsung" price="35000$" color="SkyBlue"></Device>
+      <Device  device="Laptop" model="HP VivoBook" brand="HP" price="65000$" color="Black"></Device>
+      <Device  device="SmartWatch" model="XinXI Cobee C1 Pros" brand="Cobee" price="3900$" color="Black"></Device>
+      <Device  device="HeadPhones" model="DX DigitalX" brand="Digital X" price="1550$" color="Whitish Black"></Device>
 
 
    
@@ -24,11 +26,48 @@ function App() {
   )
 }
 
+function Device (props) {
+  return (
+    <div className='device'>
+      <p>Device: {props.device} </p>
+      <p>Model: {props.model} </p>
+      <p>Brand: {props.brand} </p>
+      <p>Price: {props.price} </p>
+      <p>Color: {props.color} </p>
+    </div>
+
+  )
+}
+
+function Developer(props) {
+  console.log(props)
+  return (
+    <div style={{
+      border: '2px solid green',
+      borderRadius: '20px',
+      marginTop: '10px'
+    }}>
+      <h3>
+        Developer: {props.name}
+      </h3>
+      <p>
+        Technology: {props.tech}
+      </p>
+    </div>
+  )
+}
+
 function Person () {
   const age = 17 ; 
   const name = " Arman Mir "; 
+  const personStyle = {
+    color: 'red',
+    textAlign: 'right'
+  }
+
   return (
-    <p> I am a person{name} {age} </p>
+    <p id='' title='tooltip' style={personStyle} > I am a person{name} {age} </p>
+    
   )
 }
 function Pet() {
@@ -60,6 +99,21 @@ function Sports () {
       </ul>
       <p>
         Playing & loosing
+      </p>
+    </div>
+  )
+}
+
+
+
+function Student (){
+  return (
+    <div className='student'>
+      <p>
+        Name: 
+      </p>
+      <p>
+        Dept: 
       </p>
     </div>
   )
